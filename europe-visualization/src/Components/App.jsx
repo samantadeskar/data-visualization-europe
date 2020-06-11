2013193f8a7f5ff9e860d9128bb2de848b12f9da
Samanta Deskar <samanta.deskar@gmail.com>
2020-06-11 15:43:50 +0200

    Initial commit

---
+++ europe-visualization/src/Components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Map from "./Map/Map";
+import Navbar from "./Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Navbar />
+      <Map /> */}
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ europe-visualization/src/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+    widht: 1000,
+    height: 1000,
+}
+
+export default constants;
