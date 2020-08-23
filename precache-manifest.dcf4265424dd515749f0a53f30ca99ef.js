60bc46138ec4e5e2cdbea194141e42d9b926e698
Samanta Deskar <samanta.deskar@gmail.com>
2020-08-23 22:48:00 +0200

    Updates

---
+++ precache-manifest.dcf4265424dd515749f0a53f30ca99ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8d499f60af871aa203643fed4a27b3",
+    "url": "/data-vizualization-europe/index.html"
+  },
+  {
+    "revision": "24f6940d38d93f29c578",
+    "url": "/data-vizualization-europe/static/css/main.2c3acc43.chunk.css"
+  },
+  {
+    "revision": "1182a5ccbbbfee521ce6",
+    "url": "/data-vizualization-europe/static/js/2.ef93116a.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/data-vizualization-europe/static/js/2.ef93116a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24f6940d38d93f29c578",
+    "url": "/data-vizualization-europe/static/js/main.406ac283.chunk.js"
+  },
+  {
+    "revision": "9becb47f953a57c2c2c4",
+    "url": "/data-vizualization-europe/static/js/runtime-main.87c4a61a.js"
+  }
+]);
